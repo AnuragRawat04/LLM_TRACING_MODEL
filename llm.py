aace87857b4fa1224b96582c3135aa4cd6710edb
Anurag Rawat <135039616+AnuragRawat04@users.noreply.github.com>
2026-01-14 16:29:04 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,7 +1,7 @@
 from google import genai
-
-client = genai.Client()
-
+from dotenv import load_dotenv
+load_dotenv()
+client = genai.Client(api_key=os.getenv("Gemini_api_key"))
 def call_gemini(prompt: str, model_name: str):
     response = client.models.generate_content(
         model=model_name,
