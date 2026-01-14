b0fed34b94fc548e44cb00c14c3da2b1d4e54d7f
Anurag Rawat <135039616+AnuragRawat04@users.noreply.github.com>
2026-01-14 16:36:46 +0530

    Import os module for environment variable access

--- llm.py
+++ llm.py
@@ -1,5 +1,7 @@
 from google import genai
 from dotenv import load_dotenv
+import os
+
 load_dotenv()
 client = genai.Client(api_key=os.getenv("Gemini_api_key"))
 def call_gemini(prompt: str, model_name: str):
